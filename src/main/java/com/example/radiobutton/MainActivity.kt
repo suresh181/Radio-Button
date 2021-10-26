6136bbb9e61885835f2f1183542edaffb62e5d88
B Suresh <80473048+suresh181@users.noreply.github.com>
2021-10-26 09:00:07 +0530

    Add files via upload

---
+++ src/main/java/com/example/radiobutton/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.radiobutton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.RadioButton
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnOrder.setOnClickListener {
+            val checkedMeatRadioButtonId = rgMeat.checkedRadioButtonId
+            val meat = findViewById<RadioButton>(checkedMeatRadioButtonId)
+            val cheese = cbCheese.isChecked
+            val onions = cbOnions.isChecked
+            val salad = cbSalad.isChecked
+            val orderString = "You Order a burger with \n" +
+                    "${meat.text}" +
+                    (if (cheese) "\nCheese" else "") +
+                    (if (onions) "\nOnions" else "") +
+                    (if (salad) "\nSalad" else "")
+            tvOrder.text = orderString
+        }
+
+
+    }
+}
